d51eaba051887126de5b0d445379e2159ba5515e
shazil73 <kaifzahidsyed@gmail.com>
2025-06-30 06:21:16 -0700

    nice

---
+++ lesson 25/demo.js
@@ -0,0 +1,46 @@
+var a = 6  // number
+var b = 7.6  // number
+var c = null  // object
+var d = "Hello, World!"  //string
+var e = undefined  // undefined
+var f = true  //boolean
+var g = false  //boolean
+var h = {color: "red", size: 10, shape: "circle"}  // object
+
+console.log(typeof(e))
+
+var i = new Date()
+var j = new Date().getDay()
+console.log(i)
+console.log(j)
+
+while(a < 20) {
+    console.log(a)
+    a = a + 1
+}
+
+let k = 10
+
+const l = 20
+
+{
+    var m = 30
+}
+
+console.log(m)
+
+//{
+//let n = 40
+//}
+  //  console.log(n)
+
+//{
+//const o = 50
+//}
+//console.log(o)  
+
+//const p = 'i am shazil'
+//p = 'i am not shazil'
+
+//console.log(p)
+
